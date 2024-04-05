0766ab8f8a27152505a0a1fa511025ca3add45c1
edgar-nbc <2512@newbitcoincity.com>
2024-04-05 10:11:17 +0700

    model trainer

--- src/mlProject/pipeline/stage_03_data_transformation.py
+++ src/mlProject/pipeline/stage_03_data_transformation.py
@@ -1,6 +1,7 @@
 from mlProject.config.configuration import ConfigurationManager
 from mlProject.components.data_transformation import DataTransformation
 from mlProject import logger
+from pathlib import Path
 
 STAGE_NAME = "Data Transformation stage"
 
@@ -9,10 +10,19 @@ class DataTransformationTrainingPipeline:
         pass
 
     def main(self):
-        config = ConfigurationManager()
-        data_transformation_config = config.get_data_transformation_config()
-        data_transformation = DataTransformation(config=data_transformation_config)
-        data_transformation.train_test_spliting()
+        try:
+            with open(Path("artifacts/data_validation/status.txt"),'r') as f:
+                status=f.read().split(" ")[-1]
+            if status=="True":
+                config = ConfigurationManager()
+                data_transformation_config = config.get_data_transformation_config()
+                data_transformation = DataTransformation(config=data_transformation_config)
+                data_transformation.train_test_spliting()
+            else:
+                raise Exception("Your data schema is not valid")
+        
+        except Exception as e:
+            print(e)
 
 if __name__=='__main__':
     try:
